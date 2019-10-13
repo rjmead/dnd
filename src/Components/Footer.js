5c3a97cbf9febb06afde75e3d076397f7a327171
bmead92 <bmead92@gmail.com>
2019-10-13 10:14:30 -0400

    Update Modal Layout

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer className="App-footer">
-            <p>
                 <b>Not Working? <a href="http://dnd-api-resource.herokuapp.com">Click Here</a></b>
-            </p>
         </footer>
     )
 }
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -4,16 +4,16 @@ function Modal(props) {
     const showHideClassName = props.show ? "modal display-block" : "modal display-none";
     let tableRows = []
 
-    if(props.data){
+    if (props.data) {
         // create array of table rows agnostic of spells/equipment
-        for(let propName in props.data){
-            if(!props.data.hasOwnProperty(propName)) continue
+        for (let propName in props.data) {
+            if (!props.data.hasOwnProperty(propName)) continue
             let key = propName
             //escape keys we dont need
-            if("_id index school subclasses url".indexOf(key) !== -1) continue
+            if ("_id index school subclasses url name desc".indexOf(key) !== -1) continue
             let val = props.data[key]
             // massaging data
-            switch(key){
+            switch (key) {
                 case "desc":
                     val = val.join(" ")
                     let find = "â€™";
@@ -28,22 +28,33 @@ function Modal(props) {
                     val = val.join("/")
                     break
                 default:
-                    if(typeof val !== "string") val = <pre>{JSON.stringify(val, undefined, 2)}</pre>
+                    if (typeof val !== "string") val = <pre>{JSON.stringify(val, undefined, 2)}</pre>
                     break
             }
-            tableRows.push(<tr key={key}><td>{key}</td><td>{val}</td></tr>)
+            tableRows.push(<tr key={key}>
+                <td>{key}</td>
+                <td>{val}</td>
+            </tr>)
         }
     }
 
     return (
         <div className={showHideClassName}>
             <section className="modal-main">
-                <table>
+                <div className="ui message huge">
+                    <div className="ui block header">
+                        {props.data != null ? props.data.name : null}
+                    </div>
+                    <p>
+                        {props.data != null ? props.data.desc : null}
+                    </p>
+                </div>
+                <table className="ui large red celled striped table">
                     <tbody>
-                        {tableRows}
+                    {tableRows}
                     </tbody>
                 </table>
-                <button onClick={props.handleClose}>close</button>
+                <button className="ui button red" onClick={props.handleClose}>close</button>
             </section>
         </div>
     )
